3bfd910eb4015b24522f8afda34e3a842bb70acc
Nicholas Denton-Cheng <68447872+nicholasjdc@users.noreply.github.com>
2022-06-03 14:42:04 -0400

    txt

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:async/async.dart';
 
 void main() {
   runApp(MyApp());
--- lib/models/folder.dart
+++ lib/models/folder.dart
@@ -1,7 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:quotaline_application/models/quote.dart';
+
 class Folder {
-  String folderName = '';
-  List<String> quotes = [];
-  List<Folder> folders = [];
+  final String id;
+  final String folderName;
+  final List<Quote> quotes;
+  final List<Folder> folders;
+  Folder({
+    required this.id,
+    required this.folderName,
+    required this.quotes,
+    required this.folders
+  });
 
   List<String> getFolderNames() {
     List<String> folderNames = [];
@@ -10,6 +20,11 @@ class Folder {
     }
     return folderNames;
   }
-}
 
-Folder rootFolder = new Folder();
+Folder.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
+      : id = doc.id,
+       folderName = doc.data()!["folderName"],
+        quotes = doc.data()!["quotes"].cast<Quote>(),
+        folders = doc.data()!["folders"].cast<Folder>();
+}
+Folder rootFolder = new Folder(id: 'root',folderName: 'root',quotes: [],folders: []);
